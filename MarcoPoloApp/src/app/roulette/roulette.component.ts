ac5cd0104405338a48914a29647e05ce3603748f
axel_kde <axel.derbisz@epitech.eu>
2023-03-18 20:06:20 +0100

    feat/master: deleted old wheel

--- MarcoPoloApp/src/app/roulette/roulette.component.ts
+++ MarcoPoloApp/src/app/roulette/roulette.component.ts
@@ -1,35 +1,10 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NgxWheelComponent } from 'ngx-wheel';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-spin-wheel',
-  template: `
-    <div class="wheel-container">
-      <ngx-wheel #wheel [segments]="segments"></ngx-wheel>
-      <button (click)="spinWheel()">Spin</button>
-    </div>
-  `,
-  styles: [`
-    .wheel-container {
-      position: relative;
-      width: 500px;
-      height: 500px;
-    }
-
-    button {
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      padding: 12px 24px;
-      font-size: 24px;
-      background-color: #3498db;
-      border: none;
-      color: white;
-      cursor: pointer;
-    }
-  `]
+  selector: 'app-roulette',
+  templateUrl: './roulette.component.html',
+  styleUrls: ['./roulette.component.css']
 })
 export class RouletteComponent {
-  
+
 }
